b8f7cd7a08ff7b66ca346b016d7d49702a0f3bd6
MihirMishra23 <48871899+MihirMishra23@users.noreply.github.com>
2025-04-27 10:29:54 -0400

    first attempt at EndtoEndRAG

--- src/model.py
+++ src/model.py
@@ -4,37 +4,44 @@ import torch.nn as nn
 import faiss
 import numpy as np
 from datasets import load_dataset
-from src.generator.generator import Generator
-from retriever import Retriever
+from generator.generator import RAGGenerator
+from retriever.retriever import Retriever
 
 VD_PATH = ""
 DOCUMENT_PATH = ""
 
 
 class EndtoEndRAG(nn.Module):
-    def __init__(self):
+    def __init__(self, vd_path=VD_PATH, document_path=DOCUMENT_PATH, device="cpu"):
         """
+        Initializes the RAG model with a retriever and a generator.
 
+        Args:
+            vd_path (str): Path to the vector database (for retriever).
+            document_path (str): Path to the document store.
+            device (str): Device to run the models on ('cpu' or 'cuda').
         """
+        super().__init__()
+        self.retriever = Retriever(
+            vd_path=vd_path, document_path=document_path, device=device
+        )
+        self.generator = RAGGenerator(device=device)
 
-        ## TODO
-        ################
-        self.retriever = Retriever
+    def forward(self, query, k=5):
+        """
+        Args:
+            query (str): The input query.
+            k (int): Number of documents to retrieve.
+        Returns:
+            List[str]: Generated answers for each retrieved document.
+        """
+        # Retrieve top-k documents for the query
+        docs = self.retriever(query, k)
 
-        self.generator = Generator
+        # Generate an answer for each (query, doc) pair
+        answers = []
+        for doc in docs:
+            answer = self.generator.generate(question=query, context=doc)
+            answers.append(answer)
 
-       
-
-    def forward(self, x):
-
-        ## TODO
-        #################
-
-        z = self.retriever(x)
-
-        out = self.generator(x, z)
-
-        #################
-
-        pass
-
+        return answers
