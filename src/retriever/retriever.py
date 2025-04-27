5968ccafe4524da918c6bd23a31ab117e6ded521
MihirMishra23 <48871899+MihirMishra23@users.noreply.github.com>
2025-04-27 10:30:37 -0400

    made minor changes, added parameters to init

--- src/retriever/retriever.py
+++ src/retriever/retriever.py
@@ -6,36 +6,33 @@ import numpy as np
 from datasets import load_dataset
 from QueryEncoder import BERTQueryEncoder
 
+
 class Retriever(nn.Module):
-    def __init__(self):
-        """
-        """
-        # TODO
-        # initializes database:
-        xb = np.load(VD_PATH)
+    def __init__(self, vd_path, document_path, device="cpu"):
+        """ """
+        # TODO: initialize the database and the query encoder
+        super().__init__()
+        xb = np.load(vd_path)
         self.d = xb.shape[1]
-        self.index = faiss.IndexFlatL2(self.d)   
+        self.index = faiss.IndexFlatL2(self.d)
         print(self.index.is_trained)
-        self.index.add(xb)                  
+        self.index.add(xb)
         print(self.index.ntotal)
-        self.corpus = load_dataset("rag-datasets/rag-mini-bioasq", "text-corpus")['passages']
+        self.corpus = load_dataset("rag-datasets/rag-mini-bioasq", "text-corpus")[
+            "passages"
+        ]
 
         self.q = BERTQueryEncoder()
 
     def forward(self, x, k):
-        """
-        
-        """
+        """ """
         # TODO
         # This should be the embedding of the query:
         xq = self.q(x)
 
         # I is the indices of the top k documents
-        D, I = self.index.search(xq, k)  
+        D, I = self.index.search(xq, k)
 
         docs = self.corpus[I]
 
         return x + docs
-
-
-
