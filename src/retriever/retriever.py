8875629e2049da3a2f60a2048018fc3392e496ea
MihirMishra23 <48871899+MihirMishra23@users.noreply.github.com>
2025-04-28 23:03:43 -0400

    updated data loading logic

--- src/retriever/retriever.py
+++ src/retriever/retriever.py
@@ -6,30 +6,45 @@ import numpy as np
 from datasets import load_dataset
 import sys
 import os
+from datasets import Dataset
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../src/retriever'))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../src/retriever"))
 
 from QueryEncoder import BertQueryEncoder
 
 
 class Retriever(nn.Module):
-    def __init__(self, vd_path, document_path, device="cpu"):
-        """ """
+    def __init__(self, vd_path: str, dataset: Dataset, device: str = "cpu"):
+        """
+        Args:
+            vd_path: path to the vector database - should be a faiss index
+            dataset: dataset of documents
+            device: device to run the model on
+        """
         # TODO: initialize the database and the query encoder
         super().__init__()
 
-        data = torch.load(vd_path)
-        tensor = data['embeddings'] 
-        
-        xb = tensor.cpu().numpy()
-        self.d = xb.shape[1]
-        self.index = faiss.IndexFlatL2(self.d)
-        print(self.index.is_trained)
-        self.index.add(xb)
-        print(self.index.ntotal)
-        self.corpus = load_dataset("rag-datasets/rag-mini-bioasq", "text-corpus")[
-            "passages"
-        ]
+        # Load the embeddings
+        if vd_path.endswith(".faiss"):
+            print("Loading FAISS index...")
+            self.index = faiss.read_index(vd_path)
+            print("Successfully loaded FAISS index")
+            print(f"Index size: {self.index.ntotal}")
+            print(f"Index dimension: {self.index.d}")
+        elif vd_path.endswith(".pt"):
+            data = torch.load(vd_path)
+            embeddings = data["embeddings"]
+            # Convert to FAISS index
+            print("\nConverting to FAISS index...")
+            embeddings_np = embeddings.numpy().astype("float32")
+            dimension = embeddings.shape[1]
+            index = faiss.IndexFlatL2(dimension)
+            index.add(embeddings_np)
+            self.index = index
+        else:
+            raise ValueError(f"Unsupported file type: {vd_path.split('.')[-1]}")
+
+        self.dataset = dataset
 
         self.q = BertQueryEncoder()
 
@@ -42,7 +57,7 @@ class Retriever(nn.Module):
 
         # I is the indices of the top k documents
         D, I = self.index.search(xq.detach(), k)
-        
-        docs = [[self.corpus[int(i)] for i in row] for row in I]
+
+        docs = [[self.dataset[int(i)] for i in row] for row in I]
         print(docs)
         return docs
